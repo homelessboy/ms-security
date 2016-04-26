54e18be3e3db853f1f0ed82cc1623544ea6fb049
xuhui <xuhui@simpletour.com>
2016-04-26 18:31:01 +0800

    配置测试

--- security/src/test/java/me/afsd/ms/security/service/ModuleServiceTest.java
+++ security/src/test/java/me/afsd/ms/security/service/ModuleServiceTest.java
@@ -1,11 +1,18 @@
 package me.afsd.ms.security.service;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import me.afsd.ms.security.SecurityApplication;
+import me.afsd.ms.security.domain.Module;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 /**
  * @author XuHui (416422546@qq.com)
@@ -13,13 +20,44 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(SecurityApplication.class)
+@Transactional
+@Rollback(false)
 public class ModuleServiceTest {
     @Autowired
     ModuleService moduleService;
 
+    private Long id;
+
+    @Before
+    public void setUp(){
+        Module module=new Module();
+        module.setName("模块1");
+        module.setDomain("/module1");
+        module.setCode("module1");
+        Optional<Module> moduleOptional=moduleService.save(module);
+        id=moduleOptional.get().getId();
+    }
+
     @Test
     public void test(){
+        System.out.println(id);
         System.out.println("in");
         System.out.println(moduleService.getDomainName());
+        System.out.println(moduleService.getOne(id).get().getName());
+        Module module=moduleService.getOne(id).get();
+        Integer version=module.getVersion();
+        module.setName("hello2");
+        System.out.println(version);
+        module.setVersion(module.getVersion()+3);
+        System.out.println(moduleService.saveAndFlush(module).get().getVersion());
+    }
+
+    @Test
+    public void test1(){
+        Module module=new Module();
+        module.setId(id);
+        module.setVersion(10);
+        moduleService.save(module);
+        System.out.println();
     }
 }
